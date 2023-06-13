6d93346c39a13c2ff6276321f9d0d5f404bf1419
Nuwanga Rajapaksha <57533167+nuwangarajapaksha@users.noreply.github.com>
2023-06-14 00:28:22 +0530

    Add files via upload

---
+++ src/NewClass.java
@@ -0,0 +1,35 @@
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.tree.DefaultTreeCellEditor;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author EZIO AUDITORY
+ */
+public class NewClass {
+
+    void assertTrue(boolean condition){
+        System.out.println(condition);
+    }
+    
+    public static void main(String[] args) {
+        String x = "455456.44";
+        int u = Integer.parseInt(x);
+        System.out.println(u);
+    NewClass n = new NewClass();
+          n.assertTrue(true); 
+    }
+    
+    JTextField n = new JTextField();
+    
+    JLabel l = new JLabel();
+    
+
+}
---
+++ test/NewClassTest.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author EZIO AUDITORY
+ */
+public class NewClassTest {
+    
+    public NewClassTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class NewClass.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        NewClass.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+        
+        assertTrue(true);
+    }
+    
+    
+}
